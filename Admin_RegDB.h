1d3a8c04cf5d7a2c4af84ee214aa9572c4a2eea4
Abhishek164 <abhishekrajrathor2@gmail.com>
2016-06-07 11:12:16 +0000

    Add files via upload

---
+++ Admin_RegDB.h
@@ -0,0 +1,13 @@
+//
+//  Admin_RegDB.h
+//  RGI_Bhopal
+//
+//  Created by Abhishek on 5/11/16.
+//  Copyright (c) 2016 Technologies. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Admin_RegDB : NSObject
+
+@end
