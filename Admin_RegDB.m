71c928f325fb7bf7f44038fec44d3807c922c98c
Abhishek164 <abhishekrajrathor2@gmail.com>
2016-06-07 11:10:09 +0000

    Add files via upload

---
+++ Admin_RegDB.m
@@ -0,0 +1,13 @@
+//
+//  Admin_RegDB.m
+//  RGI_Bhopal
+//
+//  Created by Abhishek on 5/11/16.
+//  Copyright (c) 2016 Technologies. All rights reserved.
+//
+
+#import "Admin_RegDB.h"
+
+@implementation Admin_RegDB
+
+@end
